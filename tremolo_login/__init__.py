3de98bed9a66712109229de485c63a3acbaa9705
nggit <contact@anggit.com>
2026-01-31 10:35:12 +0700

    release 1.1.0

--- tremolo_login/__init__.py
+++ tremolo_login/__init__.py
@@ -12,7 +12,7 @@ from base64 import b64decode, urlsafe_b64encode as b64encode
 
 from tremolo.exceptions import BadRequest, Forbidden
 
-__version__ = '1.0.11'
+__version__ = '1.1.0'
 __all__ = ['Session']
 
 
